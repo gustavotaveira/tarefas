a45b6f85e79f26371b84c6c8d271ca653a032976
Gustavo Vinicius Taveira Lima <gustavo_vinicius_taveira@hotmail.com>
2019-02-19 23:35:22 -0300

    Adicionando autorizacao ao sistema web

---
+++ src/br/com/home/interceptors/AutorizadorInterceptor.java
@@ -0,0 +1,25 @@
+package br.com.home.interceptors;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AutorizadorInterceptor extends HandlerInterceptorAdapter {
+
+    public static final String LOGIN = "/login";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+        if (requestURI.contains(LOGIN)) {
+            return true;
+        }
+        if (request.getSession().getAttribute("usuarioLogado") != null) {
+            return true;
+        }
+
+        response.sendRedirect("/tarefas/login");
+        return false;
+    }
+}
